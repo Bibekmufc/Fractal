be4675cd936edc3aefc9a296cb674828130a9e58
Bibek Gautam <bibekmufc21@gmail.com>
2018-03-10 15:42:36 +0545

    Added HSB Class.

--- Fractal/Form1.Designer.cs
+++ Fractal/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Fractal
 {
-    partial class Form1
+    partial class Fractal
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.mandlebrot_img = new System.Windows.Forms.PictureBox();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mandlebrot_img)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menu
+            // 
+            this.menu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(1208, 28);
+            this.menu.TabIndex = 0;
+            this.menu.Text = "menuStrip1";
+            // 
+            // mandlebrot_img
+            // 
+            this.mandlebrot_img.BackColor = System.Drawing.Color.Black;
+            this.mandlebrot_img.Location = new System.Drawing.Point(0, 27);
+            this.mandlebrot_img.Name = "mandlebrot_img";
+            this.mandlebrot_img.Size = new System.Drawing.Size(1208, 483);
+            this.mandlebrot_img.TabIndex = 1;
+            this.mandlebrot_img.TabStop = false;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // Fractal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1208, 564);
+            this.Controls.Add(this.mandlebrot_img);
+            this.Controls.Add(this.menu);
+            this.MainMenuStrip = this.menu;
+            this.Name = "Fractal";
             this.Text = "Form1";
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mandlebrot_img)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.PictureBox mandlebrot_img;
     }
 }
 
--- Fractal/Form1.cs
+++ Fractal/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Fractal
 {
-    public partial class Form1 : Form
+    public partial class Fractal : Form
     {
-        public Form1()
+        public Fractal()
         {
             InitializeComponent();
         }
